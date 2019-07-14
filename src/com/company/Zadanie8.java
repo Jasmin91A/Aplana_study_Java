6f5bc05059dd22cabc02cb8ff46de5532a78559e
Jasmin91A <jasmin199111@yandex.ru>
2019-07-14 16:30:34 +0300

    add 8 task

---
+++ src/com/company/Zadanie8.java
@@ -0,0 +1,13 @@
+package com.company;
+
+    public class Zadanie8 {
+        public static void main(String[] args) {
+            int num = 1;
+            while (num <= 50)
+            {
+                System.out.println(num);
+                num++;
+            }
+        }
+    }
+
