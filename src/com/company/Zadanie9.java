80ac3a54c6f95725bac841eb1dec63e2a861b838
Jasmin91A <jasmin199111@yandex.ru>
2019-07-20 22:59:01 +0300

    added 9 tasks

--- src/com/company/Zadanie9.java
+++ src/com/company/Zadanie9.java
@@ -3,24 +3,23 @@ package com.company;
 import java.util.Scanner;
 
 public class Zadanie9 {
-    public static void main(String[] agrs)
-    {
-        int i = 0;
-        int a;
+    public static void main(String[] agrs) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите длину массива: ");
+            int a = in.nextInt();
+            int num[] = new int[a];
 
-        Scanner sc = new Scanner(System.in);
-            System.out.print("Введите длину массива: ");
-            a = sc.nextInt();
+            System.out.println("Ведите значения массива:");
+            for (int i = 0; i < a; i++)
+            {
+                num[i] = in.nextInt () * 2;
+            }
 
-        int [] num = new int [a];
+            System.out.println ("Значения в массиве:");
+            for (int i = 0; i < a; i++)
+            {
+                System.out.println (num[i]);
+            }
 
-       for (int ignored : num) {
-           System.out.print("Введите " + (i + 1) + "-й элемент массива: ");
-            num[i] = sc.nextInt();
-
-            System.out.println(num[i] + "*2" + "=" + num[i]*2);
-
-            i++;
         }
     }
-}
