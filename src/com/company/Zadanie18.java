e7401478b9c31a7ced688b70f1459a4afea7c58d
Jasmin91A <jasmin199111@yandex.ru>
2019-08-10 19:45:26 +0300

    added 18 tasks

--- src/com/company/Zadanie18.java
+++ src/com/company/Zadanie18.java
@@ -5,17 +5,17 @@ import java.io.*;
     public class Zadanie18 {
         public static void main(String[] args) {
             String r;
-            String w;
             int count = 0;
 
-            try (BufferedReader br = new BufferedReader(new FileReader("/Users/jasmin_91/Desktop/Zadanie16"))) {
-                LineNumberReader lineReader = new LineNumberReader(br);
-                while (lineReader.readLine() != null) {
-                    count++;
-                }
-
-                while ((r = br.readLine()) != null) {
-                    System.out.println(r);
+            try
+            {
+                FileInputStream fstream = new FileInputStream("/Users/jasmin_91/Desktop/Zadanie16");
+                BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+                String strLine;
+                while ((strLine = br.readLine()) != null)
+                {
+                    System.out.println(strLine);
+                    {++count;}
                 }
 
             } catch (IOException exc) {
@@ -28,17 +28,16 @@ import java.io.*;
             System.out.println("Чтобы завершить ввод данных - пропишите команду 'stop' ");
 
             try (FileWriter fw = new FileWriter("/Users/jasmin_91/Desktop/Zadanie16")) {
-                do {
-                    System.out.print(": ");
-                    w = br.readLine();
-
-                    if (w.compareTo("stop") == 0) break;
-                    w = w + "\r\n";
-                    fw.write(w);
+                while (count!=0){
+                    System.out.println("");
+                    r = br.readLine();
+                    r = r +"\r\n";
+                    fw.write(r);
+                    count--;
                 }
-                while (w.compareTo("stop") != 0);
+            }
 
-            } catch (IOException exc) {
+            catch (IOException exc) {
                 System.out.println("Ошибка ввода-вывода: " + exc);
             }
         }
